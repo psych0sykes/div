5fdf81f0636c3347ea477c3de79c29f943d799ae
Blake Sykes <blakesykes13@gmail.com>
2019-12-08 16:14:04 -0700

    Fixed db name in config.json

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
       res.render("index");
     });
-  };
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
