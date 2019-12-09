57d38e94e5cdf3a1e0413e44966c8039543d29f9
Blake Sykes <blakesykes13@gmail.com>
2019-12-08 22:09:12 -0700

    started on log in

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+$("#logInButton").click(function(){
+    // Collect user credentials from form
+    var logInEmail = $("#logInEmail").val();
+    var logInPassword = $("#logInPassword").val();
+
+    // Check db for user email
+
+
+})
